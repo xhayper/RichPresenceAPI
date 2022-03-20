d7adbd3115a64e29163a27f5a103d3b7f6597295
xhayper <hayper1919@gmail.com>
2022-03-20 17:28:53 +0700

    Wo

--- RichPresenceAPI/Utility.cs
+++ RichPresenceAPI/Utility.cs
@@ -7,13 +7,13 @@ namespace RichPresenceAPI
 {
     public static class Utility
     {
-        public static DiscordRpcClient createDiscordRpcClient(string applicationID, int pipe = -1, ILogger logger = null, bool autoEvents = true) {
+        public static DiscordRpcClient CreateDiscordRpcClient(string applicationID, int pipe = -1, ILogger logger = null, bool autoEvents = true) {
             return new DiscordRpcClient(applicationID, pipe, logger, autoEvents, new UnityNamedPipe());
         }
 
-        [Obsolete("Use createDiscordRpcClient instead")]
+        [Obsolete("Use CreateDiscordRpcClient instead")]
         public static DiscordRpcClient createClient(string applicationID, int pipe = -1, ILogger logger = null, bool autoEvents = true) {
-            return createDiscordRpcClient(applicationID, pipe, logger, autoEvents);
+            return CreateDiscordRpcClient(applicationID, pipe, logger, autoEvents);
         }
     }
 }
